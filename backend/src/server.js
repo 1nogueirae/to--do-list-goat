d9ffa56120f556367ac07da1a63d7e6bd818488c
1nogueirae <contato.emanuelucas@gmail.com>
2026-04-05 02:36:30 -0300

    Revert "feat: add CORS support for handling cross-origin requests"
    This reverts commit 173465284c4ff2c1fc485e82261af4b2747a22ca.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,11 @@
 require('dotenv').config();
 
-const cors = require('cors')
 const tasksRoute = require('./routes/tasks.route')
 
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({origin: 'http://localhost:5173'}))
 app.use(express.json())
 
 app.use('/tasks', tasksRoute)
